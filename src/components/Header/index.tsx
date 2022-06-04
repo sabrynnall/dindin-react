18b7ec09cebf2fdce0cb5387e846d6240d00205c
sabrynnall <limasabrynna03@gmail.com>
2022-06-04 12:40:19 -0300

    iniciando projeto

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,17 @@
+import Link from '../Link';
+import './styles.css';
+
+function Header() {
+    return (
+        <header>
+            <p>Din Din</p>
+            <nav>
+                <Link texto='Cursos' redirect='/cursos'/>
+                <Link texto='Blog' redirect='/blog'/>
+                <Link texto='Contato' redirect='/contato'/>
+            </nav>
+        </header>
+    );
+}
+
+export default Header;
